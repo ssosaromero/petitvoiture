90855e7464908d552e51423491af58e01cbe9599
Joel Jaldin <jsejaldin@gmail.com>
2022-08-08 21:17:40 -0300

    a ver

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_08_02_015250) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
     t.text "description"
